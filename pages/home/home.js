60c6eaf4b107849f7787e241a00eb565d33177df
coderzry <18348738308@163.com>
2020-11-23 19:55:59 +0800

    0303_小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,28 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Coderzry',
+    age: 18,
+    students: [
+      {id:1, name: 'a', age: 18},
+      {id:2, name: 'b', age: 19},
+      {id:3, name: 'c', age: 20},
+      {id:4, name: 'd', age: 21}
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 1.错误做法，界面是不会刷新的
+    // this.data.counter+= 1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1 
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
