dd773c1e7595a86ad6d71229bb982de3e5d9739b
coderzry <18348738308@163.com>
2020-11-29 15:31:14 +0800

    1010_网络请求及封装

---
+++ service/network.js
@@ -0,0 +1,28 @@
+// 简洁写法
+export default function request(options) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: options.url,
+      method: options.method || 'get',
+      data: options.data || {},
+      success: resolve,
+      fail: reject
+    })
+  })
+}
+
+// export default function request(options) {
+//   return new Promise((resolve, reject) => {
+//     wx.request({
+//       url: options.url,
+//       method: options.method || 'get',
+//       data: options.data || {},
+//       success: function(res) {
+//         resolve(res)
+//       },
+//       fail: function(err) {
+//         reject(err)
+//       }
+//     })
+//   })
+// }
